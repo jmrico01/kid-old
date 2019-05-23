af2516893e19dac1d858105c38a328c5b924d1df
Jose M Rico <jrico@princeton.edu>
2019-05-22 21:42:02 -0400

    cloc for lines of code

--- misc-scripts/lines_of_code.bat
+++ misc-scripts/lines_of_code.bat
@@ -1,22 +1,3 @@
-:: This batch file reads cpp and h files recursively and counts number of lines
-
-@echo off
-
-set BAT_PATH=%~dp0
-set BAT_PATH=%BAT_PATH:~0,-1%
-set SRC_PATH=%BAT_PATH%\..\src
-
-setlocal
-echo off
-set /a totalNumLines = 0
-SETLOCAL ENABLEDELAYEDEXPANSION
-
-pushd %SRC_PATH%
-for /r %%f in (*.cpp *.h *.vert *.frag ) do (
-for /f %%C in ('Find /V /C "" ^< %%f') do set Count=%%C
-echo !Count! lines in %%f
-set /a totalNumLines+=!Count!
-)
-echo.
-echo ==== Total: %totalNumLines% lines of code
-popd
+D:\Development\cloc\cloc-1.82.exe ^
+    --include-lang="C++","C/C++ Header","GLSL","Objective C++" ^
+    .\src\* .src\shaders\*
