0ff9bba2afe98dfa68fbcdcf0e42eade580d0870
Jose M Rico <jrico@princeton.edu>
2019-09-30 00:20:54 -0400

    started a panel

--- src/imgui.cpp
+++ src/imgui.cpp
@@ -1,1 +1,13 @@
 #include "imgui.h"
+
+void Panel::Begin()
+{
+}
+
+void Panel::End()
+{
+}
+
+void Panel::Text(const char* text)
+{
+}
--- src/imgui.h
+++ src/imgui.h
@@ -1,31 +1,47 @@
 #pragma once
 
-namespace km_imgui {
-
 const uint64 INPUT_BUFFER_MAX = 256;
 
 typedef FixedArray<char, INPUT_BUFFER_MAX> InputString;
 
+enum class PanelCommandType
+{
+	DRAW_TEXT
+};
+
+struct PanelCommandDataText
+{
+	FontFace* face;
+};
+
+struct PanelCommand
+{
+	PanelCommandType type;
+	union
+	{
+		PanelCommandDataText dataText;
+	};
+};
+
 struct Panel
 {
+	Vec2Int size;
+
 	void Begin();
+	void End(); // draw here?
 
 	void Text(const char* text);
 
-	bool ButtonToggle(/*...*/);
-	bool ButtonTrigger(/*...*/);
+	bool ButtonToggle();
+	bool ButtonTrigger();
 
-	float32 InputFloat32(/*...*/);
-	float32 InputSliderFloat32(/*...*/);
-	int InputInt(/*...*/);
-	int InputSliderInt(/*...*/);
-	Vec2 InputVec2(/*...*/);
-	Vec3 InputVec3(/*...*/);
-	Vec4 InputVec4(/*...*/);
-	Vec4 InputColor(/*...*/);
+	float32 InputFloat32();
+	float32 InputSliderFloat32();
+	int InputInt();
+	int InputSliderInt();
+	Vec2 InputVec2();
+	Vec3 InputVec3();
+	Vec4 InputVec4();
+	Vec4 InputColor();
 	void InputText(InputString* outText);
-
-	void End(); // draw here?
 };
-
-}
